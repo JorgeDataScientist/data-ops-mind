3dc4709b641f22ad8a8c9a3190237c3abda0a0d1
jabrahanba <jabrahanba@gmail.com>
2023-09-06 16:50:33 -0500

    reco-system

--- reco-system/main.py
+++ reco-system/main.py
@@ -5,6 +5,7 @@ from google.cloud import bigquery
 import folium
 from streamlit_folium import folium_static
 from geopy.distance import geodesic
+import json
 
 # Configura las credenciales desde las secrets de Streamlit
 credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
@@ -12,69 +13,99 @@ credentials = service_account.Credentials.from_service_account_info(st.secrets["
 # Autenticación con BigQuery
 client = bigquery.Client(credentials=credentials)
 
-# Creación de la interfaz de usuario
-st.title('Sistema de Recomendación de Empresas')
+# Función principal
+def main():
+    st.title('Sistema de Recomendación de Empresas')
 
-# Entrada de datos del usuario
-st.sidebar.header('Entrada de datos')
-my_latitude_str = st.sidebar.text_input('Latitude:', '00.00000')
-my_longitude_str = st.sidebar.text_input('Longitude:', '00.00000')
+    # Variable de estado
+    estado = st.session_state.get('estado', 'entrada_datos')
 
-# Convierte las cadenas en números de punto flotante
-my_latitude = float(my_latitude_str)
-my_longitude = float(my_longitude_str)
+    if estado == 'entrada_datos':
+        entrada_datos()
+    elif estado == 'buscar_empresas':
+        buscar_empresas()
+    elif estado == 'mostrar_reviews':
+        mostrar_reviews()
 
+def entrada_datos():
+    st.sidebar.header('Entrada de datos')
+    my_latitude_str = st.sidebar.text_input('Latitude:', '00.00000')
+    my_longitude_str = st.sidebar.text_input('Longitude:', '00.00000')
 
-rubro = st.sidebar.selectbox('line:', ['Restaurants',
-        'Shopping',
-        'Health and Beauty',
-        'Rental Services',
-        'Tourism',
-        'Entertainment',
-        'Health and Hospitals',
-        'Sports',
-        'Arts and Crafts',
-        'Events and Weddings',
-        'Automotive',
-        'Education and Learning',
-        'Veterinary and Pets',
-        'Gardening and Home Services',
-        'Technology, Networks, Electronics, and Engineering',
-        'Industry',
-        'Professional Services',
-        'Other'])
+    my_latitude = float(my_latitude_str)
+    my_longitude = float(my_longitude_str)
 
-# Modifica la seleccion para que sirva en la consulta:
-rubro = rubro.replace(" ", "").replace(",", "")
+    rubro = st.sidebar.selectbox('line:', ['Restaurants',
+            'Shopping',
+            'Health and Beauty',
+            'Rental Services',
+            'Tourism',
+            'Entertainment',
+            'Health and Hospitals',
+            'Sports',
+            'Arts and Crafts',
+            'Events and Weddings',
+            'Automotive',
+            'Education and Learning',
+            'Veterinary and Pets',
+            'Gardening and Home Services',
+            'Technology, Networks, Electronics, and Engineering',
+            'Industry',
+            'Professional Services',
+            'Other'])
 
-# Consulta a BigQuery para obtener las recomendaciones
-query = f"""
-    SELECT name, latitude, longitude, satisfaction, percent_good_reviews, identificador
-    FROM data-ops-mind.New_York.newyork_data
-    WHERE {rubro} = 1   
-"""
-#LIMIT 20
-#ORDER BY percent_good_reviews DESC
+    # Modifica la seleccion para que sirva en la consulta:
+    rubro = rubro.replace(" ", "_").replace(",", "_")
 
+    tables = {'California':'data-ops-mind.California.california_data',
+              'Florida':'data-ops-mind.Florida.florida_data', 
+              'Illinois':'data-ops-mind.Illinois.illinois_data', 
+              'New York':'data-ops-mind.New_York.newyork_data', 
+              'Texas':'data-ops-mind.Texas.texas_data'}
 
-# Ejecuta la consulta y carga los resultados en un DataFrame
-df = pd.read_gbq(query, credentials=credentials, project_id="data-ops-mind")
+    state = st.sidebar.selectbox('States:', ['California','Florida', 'Illinois', 'New York', 'Texas'])
 
-# Muestra el DataFrame
-#st.write("Resultados:")
-#st.write(df)
+    if state in tables:
+        table = tables[state]
+    else:
+        table = None
 
+    # Consulta a BigQuery para obtener las recomendaciones
+    query = f"""
+        SELECT name, latitude, longitude, satisfaction, percent_good_reviews, identificador
+        FROM {table}
+        WHERE {rubro} = 1   
+    """
 
-#ESTA PARTE BUSCA LAS MAS CERCANAS
-# Coordenadas de referencia (tus coordenadas)
-mis_coordenadas = (my_latitude, my_longitude)
-# Parámetro para la distancia
-distancia_maxima = st.sidebar.slider('Selecciona una distancia máxima (en km):', 1, 100, 10)
-# Función para calcular la distancia entre dos puntos
-def calcular_distancia(row):
-    ubicacion = (row['latitude'], row['longitude'])
-    return geodesic(mis_coordenadas, ubicacion).kilometers
-if st.sidebar.button('Buscar empresas cercanas'):
+    # Ejecuta la consulta y carga los resultados en un DataFrame
+    df = pd.read_gbq(query, credentials=credentials, project_id="data-ops-mind")
+
+    distancia_maxima = st.sidebar.slider('Selecciona una distancia máxima (en km):', 1, 100, 10)
+
+    if st.sidebar.button('Buscar empresas cercanas'):
+        st.session_state.my_latitude = my_latitude
+        st.session_state.my_longitude = my_longitude
+        st.session_state.df = df
+        st.session_state.distancia_max = distancia_maxima
+        st.session_state.table = table
+        st.session_state.rubro = rubro
+        st.session_state.estado = 'buscar_empresas'
+
+def buscar_empresas():
+    #Obtener distancia máxima:
+
+    # Obtener coordenadas
+    my_latitude = st.session_state.my_latitude
+    my_longitude = st.session_state.my_longitude
+    df = st.session_state.df
+    distancia_maxima = st.session_state.distancia_max
+    mis_coordenadas = (my_latitude, my_longitude)
+
+    # Función para calcular la distancia entre dos puntos
+    def calcular_distancia(row):
+        ubicacion = (row['latitude'], row['longitude'])
+        return geodesic(mis_coordenadas, ubicacion).kilometers
+   
     # Aplicar la función de cálculo de distancia al DataFrame
     df['Distancia'] = df.apply(calcular_distancia, axis=1)
     # Filtrar registros dentro de la distancia máxima
@@ -94,9 +125,91 @@ if st.sidebar.button('Buscar empresas cercanas'):
 
     # Mostrar los registros cercanos en Streamlit
     st.write(f'Registros cercanos dentro de {distancia_maxima} km de tus coordenadas:')
-    
-  # Agregar una columna de checkboxes al DataFrame
-    registros_cercanos['Seleccionar'] = registros_cercanos.apply(lambda row: st.checkbox("", key=row['identificador']), axis=1)
 
     # Mostrar el DataFrame con la columna de checkboxes
-    st.write(registros_cercanos)
+    registros_cercanos['Selection'] = False
+    edited = st.data_editor(registros_cercanos)
+    # Filtrar el DataFrame para obtener el registro marcado como True en 'Selection'
+    #registro_seleccionado = edited[edited['Selection'] == True]
+    # Extraer el valor de la columna 'identificador' del registro seleccionado
+    #identificador_seleccionado = registro_seleccionado.iloc[0]['identificador']
+    # Filtrar el DataFrame para obtener los registros marcados como True en 'Selection'
+    registros_seleccionados = edited[edited['Selection'] == True]
+    # Extraer los valores de la columna 'identificador' de los registros seleccionados y almacenarlos en una lista
+    identificadores_seleccionados = registros_seleccionados['identificador'].tolist()
+
+
+    if st.button('Mostrar reseñas'):
+        st.session_state.identificador = identificadores_seleccionados
+        st.session_state.estado = 'mostrar_reviews'
+
+def mostrar_reviews():
+    identificador = st.session_state.identificador
+    table = st.session_state.table 
+    rubro = st.session_state.rubro 
+
+    identificadores_str = ', '.join([f"'{id}'" for id in identificador])
+
+
+    # Consulta a BigQuery para obtener las recomendaciones
+    query2 = f"""
+        SELECT name, satisfaction, text_list, rating_list, date_list, labels
+        FROM `{table}`
+        WHERE identificador IN ({identificadores_str}) 
+    """
+    # Ejecuta la consulta y carga los resultados en un DataFrame
+    reviews = pd.read_gbq(query2, credentials=credentials, project_id="data-ops-mind")
+    # Función para extraer elementos de la lista JSON interna
+    def extract_elements(json_str, key):
+        try:
+            inner_data = json.loads(json_str)
+            inner_list = inner_data[key]["list"]
+            
+            # Filtrar elementos no nulos y obtener sus valores
+            return [element["element"] if element and "element" in element and element["element"] is not None else None for element in inner_list]
+        except:
+            return []
+
+    # Aplicar las funciones a las columnas correspondientes
+    reviews['text_list'] = reviews['text_list'].apply(extract_elements, args=("text_list",))
+    reviews['rating_list'] = reviews['rating_list'].apply(extract_elements, args=("rating_list",))
+    reviews['date_list'] = reviews['date_list'].apply(extract_elements, args=("date_list",))
+    reviews['labels'] = reviews['labels'].apply(extract_elements, args=("labels",))
+
+    # Función para combinar las reseñas, ratings y fechas
+    def combinar_review(row):
+        resenas = row['text_list']
+        ratings = row['rating_list']
+        fechas = row['date_list']
+        labels = row['labels']
+        combined_reviews = []
+        
+        for i in range(len(resenas)):
+            if ratings[i] is not None:
+                combined_reviews.append(f"📝Review: {resenas[i]}, ⭐️Rating: {ratings[i]}, 📅Date: {fechas[i]}, 💬Opinion: {labels[i]} ")
+        
+        return combined_reviews
+
+    # Aplicar la función a cada fila y crear la nueva columna
+    reviews['combined_reviews'] = reviews.apply(combinar_review, axis=1)
+
+    for i in range(reviews.shape[0]):
+        st.subheader(f'Name: {reviews.name[i]}')
+        st.write(f'Satisfaction: {reviews.satisfaction[i]}')
+           
+        if len(reviews.combined_reviews[i]) > 0:
+            for j in range(len(reviews.combined_reviews[i])):
+                st.write(f'Review: {reviews.combined_reviews[i][j]}')
+        else:
+            st.write("No reviews available.")
+
+
+
+
+    if st.button('volver'):
+        st.session_state.estado = 'buscar_empresas'
+
+
+# Ejecutar la función principal
+if _name_ == '__main__':
+    main()
